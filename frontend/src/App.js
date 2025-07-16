635e81e07283d863a83472cba76979cfdc974c50
Parley Kennelly <pdkennelly@yahoo.com>
2025-07-16 14:37:42 -0400

    Update App.js
    Add boilerplate to original empty file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,9 @@
+// app.js
+document.getElementById('threadForm').addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const url = document.getElementById('redditURL').value;
+
+  // TODO: Send URL to backend and get results
+  console.log(`Analyzing: ${url}`);
+});
 
